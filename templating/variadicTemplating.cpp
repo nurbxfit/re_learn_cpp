745c2ba486db03461a3d8157515b4fd4fb5728dd
nurbxfit <nurbxfit@gmail.com>
2024-05-26 12:02:02 +0800

    added another example for class variadic template

--- templating/variadicTemplating.cpp
+++ templating/variadicTemplating.cpp
@@ -31,8 +31,58 @@ void print(T first, Args... args)
      * */
 }
 
+/**
+ * Here is an example on how we can use the variadic tempaltes concept in a class
+ */
+
+template <typename... Args>
+class CustomArray
+{
+public:
+    CustomArray(Args... args) : size(sizeof...(Args)), elements{args...}
+    {
+    }
+
+    void print() const
+    {
+        std::cout << "[ ";
+        std::apply([this](auto &&...args)
+                   { printElements(std::forward<decltype(args)>(args)...); }, elements);
+    }
+
+private:
+    std::tuple<Args...> elements;
+    size_t size;
+
+    template <typename... Ts>
+    void printElements(Ts...) const
+    {
+    } // Empty tuple case (base case)
+
+    // here we define our Base templat
+    template <typename T>
+    void printElements(T value) const
+    {
+        std::cout << value << " ]" << std::endl;
+    }
+
+    // Here we declare the overload recursion;
+    template <typename T, typename... Rest>
+    void printElements(T first, Rest... rest) const
+    {
+        std::cout << first << ", ";
+        printElements(rest...);
+    }
+};
+
 int main()
 {
+    // exmaple using of the print function
     print(1, "Hello World", 3.142, "Somebody", "once");
+
+    // example usgin the class print method
+    CustomArray<int, double, std::string> array(1, 3.142, "Hello World");
+    array.print();
+
     return 0;
 }
